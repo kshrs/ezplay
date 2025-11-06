c39a921c1660f770779871c42d0637bce534e2fa
kishor <kishorjsk2006@gmail.com>
2025-11-06 18:56:33 +0530

    Created a Working App

--- main.go
+++ main.go
@@ -6,67 +6,105 @@ import (
 	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/widget"
 
-	"os"
-	// "fmt"
 	"log"
+	"os"
+	"os/exec"
 	"strings"
+	"errors"
 )
 
-func returnFileNames(searchString string) ([]string, error) {
-	entries, err := os.ReadDir(".")
+var dir string = "/home/kishor/Videos/Data Structures and Algorithms Design/"
+func returnFileList(searchString string) ([]string, error) {
+	entries, err := os.ReadDir(dir)
 	if err != nil {
 		return nil, err
 	}
+
 	var files []string
 	for _, entry := range entries {
-		if (!entry.IsDir()) {
-			if (strings.Contains(entry.Name(), searchString)) {
-				files = append(files, entry.Name())	
-				
-			}
+		if (!entry.IsDir() && strings.Contains(entry.Name(), searchString)) {
+			files = append(files, entry.Name())
 		}
 	}
 	return files, nil
 }
 
-func createFileView() fyne.CanvasObject {
-	label := widget.NewLabelWithStyle(
-		"Files",
-		fyne.TextAlignLeading,
-		fyne.TextStyle{Bold: true, Italic: true, Underline: true},
-	)
-	fileLabel := widget.NewLabel("")
-
-	searchText := widget.NewMultiLineEntry()
-	searchText.PlaceHolder = "Enter a text to search"
-	submit := widget.NewButton("Search", func() {
-		files, err := returnFileNames(searchText.Text)
-		if err != nil {
-			log.Panic("Error: ", err)
-		}
-		fileLabel.SetText(strings.Join(files, "\n"))
+func errorWindow(app fyne.App, msg string) {
+	w := app.NewWindow("Error Window")
+	errorLabel := widget.NewLabel("Error: " + msg)
+	closeButton := widget.NewButton("Close", func() {
+		w.Close()
 	})
-
-	return container.NewVBox(label, searchText, submit, fileLabel)
-
+	w.SetContent(container.NewVBox(errorLabel,closeButton))
+	w.Show()
 }
 
+func returnPlayCombo(fileList []string, searchString string, videoFileType string, subFileType string) (string, string, error) {
+	var videoFileName string = ""
+	var subFileName string = ""
 
-func main()  {
+	for _, file := range fileList {
+		if (strings.Contains(file, searchString) && strings.Contains(file, videoFileType)) {
+			videoFileName = file
+		}
+		if (strings.Contains(file, searchString) && strings.Contains(file, subFileType)) {
+			subFileName = file
+		}
+	}
+	if (videoFileName == "" && subFileName == "") {
+		return "", "", errors.New("No Video and Subtrack Found")
+	}
+	if (subFileName == "") {
+		return videoFileName, "", nil
+	}
+
+	return strings.Join([]string{dir, videoFileName}, ""),
+			strings.Join([]string{dir, subFileName}, ""),
+			nil
+}
+
+func main() {
 	a := app.New()
 	w := a.NewWindow("Main Window")
 
+
+	fileList, err := returnFileList("")
+	if err != nil {
+		errorWindow(a, err.Error())
+		return
+	}
+	fileListLabel := widget.NewLabel(strings.Join(fileList, "\n"))
+
+	searchEntry := widget.NewMultiLineEntry()
+	searchEntry.PlaceHolder = "Type to search files"
+	searchButton := widget.NewButton("Search", func() {
+		fileList, err := returnFileList(searchEntry.Text)
+		if err != nil {
+			errorWindow(a, err.Error())
+			return
+		}
+		fileListLabel.SetText(strings.Join(fileList, "\n"))
+	})
+
+	playButton := widget.NewButton("Play", func() {
+		videoFileName, subFileName, err := returnPlayCombo(fileList, searchEntry.Text, ".mp4", ".srt")
+		if err != nil {
+			errorWindow(a, err.Error())
+			return
+		}
+
+		playCmd := exec.Command("mpv", videoFileName, strings.Join([]string{"--sub-file", subFileName}, "="))
+		log.Println(playCmd)
+		err = playCmd.Run()
+		if err != nil {
+			errorWindow(a, "MPV Error")
+			return
+		}
+
+
+	})
 	
-	title := widget.NewLabelWithStyle(
-		"Welcome To EzPlay!",
-		fyne.TextAlignCenter,
-		fyne.TextStyle{Bold: true},
-	)
 
-	w.SetContent(container.NewVBox(
-		title,
-		createFileView(),
-	))
+	w.SetContent(container.NewVBox(searchEntry, searchButton, fileListLabel, playButton))
 	w.ShowAndRun()
-
 }
